2242273c4c19384ef1e825025ab6bb226d670cdf
hoangbuix <buihoang9b8@gmail.com>
2021-04-27 22:31:53 +0700

    add new

--- src/main/resources/sql/storeproducer.sql
+++ src/main/resources/sql/storeproducer.sql
@@ -1,49 +1,29 @@
 use inventory_management;
-drop procedure if EXISTS futureUser;
+
+
+drop procedure if EXISTS user_create;
 DELIMITER $$
-CREATE PROCEDURE futureUser(
-    -- _first_name   VARCHAR(100),
---     _last_name    VARCHAR(100) ,
---     _avatar       VARCHAR(255) ,
---     _user_name    VARCHAR(50)  ,
---     _password     VARCHAR(100) ,
---     _email        VARCHAR(100) ,
---     _active_flag  INTEGER,
---     _created_date TIMESTAMP,
---     _updated_date TIMESTAMP
-	IN _tables VARCHAR(255),
-	IN _fields TEXT,
-	IN _joins TEXT,
-	IN _condition TEXT,
-	IN _group TEXT,
-	IN _order TEXT,
-	IN _limit TEXT
+CREATE PROCEDURE user_create(
+     in _first_name   VARCHAR(100),
+     in _last_name    VARCHAR(100) ,
+     in _avatar       VARCHAR(255) ,
+     in _user_name    VARCHAR(50)  ,
+     in _password     VARCHAR(100) ,
+     in _email        VARCHAR(100) ,
+     in _active_flag  INTEGER,
+     in _created_date TIMESTAMP,
+     in _updated_date TIMESTAMP
 )
 
 body:BEGIN
-		DECLARE code char(5) default '00000';
-		DECLARE msg TEXT;
-		DECLARE rowss INT;
-		DECLARE result TEXT;
-	
-    DECLARE CONTINUE HANDLER FOR SQLEXCEPTION
-    BEGIN
-		GET diagnostics condition 1
-        code = returned_sqlstate, msg = message_text;
-    END;
-    
-		SET @sql = CONCAT('INSERT INTO', _table, ' (', _field, ') VALUES (', _values, ');');
-		prepare _query from @sql;
-		execute _query;
-    
-    if code = '00000' then
-		get diagnostics rowss = row_count;
-        set @sql = concat('select max(', _increment,') as lastID from', _table, ';');
-        prepare _query from @sql;
-        execute _query;
+	SET max_sp_recursion_depth=255;
+	if(select count(user.id) from user where user.user_name and user.email) > 0 then
+		SET @message_text = CONCAT('User name \'', user_name, '\' already exists');
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = @message_text;
     else
-		set result = concat('insert failed, error = ', code, ', message = ', msg, @sql);
-	select result as mysqlerror;
+		insert into user(first_name, last_name, avatar, user_name, password, email, active_flag, created_date, updated_date)
+        values (_first_name, _last_name, _avatar, _user_name, _password, _email, _active_flag, _created_date, _updated_date);
+        select * from user where email = _email and user_name = _user_name;
     end if;
 END$$
 DELIMITER ;
